5bc8343c9528ba6e4d182fbe0eb6bd5106d0b5fa
Ranto Billy <billyar172334@gmail.com>
2024-04-11 10:52:51 +0300

    styled-components

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,21 +1,40 @@
-
+import DefaultPicture from '../../assets/profile.png'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const CardLabel = styled.span`
+    color: #5843e4;
+    font-size: 22px;
+    font-weight: bold;
+`
+const CardImage = styled.img`
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+`
 
 function Card({label, title, picture}){
     return(
         <div style={{display:'flex', flexDirection:'column', padding:15}}>
-            <span>{label}</span>
-            <img src={picture} alt="freelance" height={80} width={80}/>
+            {/*<span>{label}</span>*/}
+            <CardLabel>{label}</CardLabel>
+            {/*<img src={picture} alt="freelance" height={80} width={80}/>*/}
+            <CardImage src={picture} alt="freelance"/>
             <span>{title}</span>
         </div>
     )
 }
 
 Card.propTypes = {
-    label: PropTypes.string,
+    label: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,       //error in the console if one of these props is used as another type than string in the other components
-    picture: PropTypes.string,                //When it's isRequired, an error will occured if the prop is not used in the other components
+    picture: PropTypes.string.isRequired,                //When it's isRequired, an error will occured if the prop is not used in the other components
 }
 
+Card.defaultProps = {
+    title: '',  // 'default title is the value if the title property is not defined in the use of the profile object'
+    label: '',
+    picture: DefaultPicture,
+}
 
 export default Card
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,11 +1,35 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+//Stylizing an element form a library
+// $ is used only for React components (e.g : Link) and not for HTML elements (cannot work with h1 or p etc ...)
+const StyledLink = styled(Link)` 
+    padding: 15px;
+    color: #8186a0;
+    text-decoration: none;
+    font-size: 18px;
+
+    ${(props) => 
+        props.$isFullLink && `
+            color:white;
+            border-radius: 30px;
+            background-color: #5843E4;
+        `
+    }
+`
 
 function Header(){
     return(
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/survey/1">Questionnaire</Link> {/** Initialization when it's the first question, will change depends on the next or previous link */}
-            <Link to="/Freelance">Freelance</Link>
+            {/*<Link to="/">Home</Link>*/}
+            <StyledLink to="/">Home</StyledLink>
+
+            {/*<Link to="/survey/1">Questionnaire</Link>*/}
+            {/** Only Questionnaire has the styles from the props */} 
+            <StyledLink to="/survey/1" $isFullLink>Questionnaire</StyledLink> {/** Initialization when it's the first question, will change depends on the next or previous link */}
+
+            {/*<Link to="/Freelance">Profiles</Link>*/}
+            <StyledLink to="/Freelance">Profiles</StyledLink>
         </nav>
     )
 }
